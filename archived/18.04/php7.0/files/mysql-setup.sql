2151d2cc3d3e919e5b0659218957312c934e5e25
Jason Moore <jason@zivtech.com>
2020-02-06 14:50:25 -0500

    Removing archived dir. Did not want to bring that from master.

--- archived/18.04/php7.0/files/mysql-setup.sql
+++
@@ -1,9 +0,0 @@
-DROP USER 'root'@'localhost';
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'strongpassword';
-CREATE USER 'root'@'127.0.0.1' IDENTIFIED BY 'strongpassword';
-CREATE USER 'root'@'::1' IDENTIFIED BY 'strongpassword';
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'127.0.0.1';
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'::1';
-CREATE DATABASE drupal;
-FLUSH PRIVILEGES;
