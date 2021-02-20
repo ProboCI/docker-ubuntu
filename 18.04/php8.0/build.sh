9445f8107b4de219124b73bd7e4f51b770648ff1
Michael Bagnall <mrbagnall@icloud.com>
2021-02-20 09:12:57 -0500

    Corrected build file

--- 18.04/php8.0/build.sh
+++ 18.04/php8.0/build.sh
@@ -2,5 +2,5 @@
 
 docker build . -t proboci/ubuntu:18.04-php8.0 --squash
 docker build . -t mbagnall/ubuntu:18.04-php8.0 --squash
-docker push mbagnall/ubuntu:18.04-php8.0
+docker push proboci/ubuntu:18.04-php8.0
 docker push mbagnall/ubuntu:18.04-php8.0
