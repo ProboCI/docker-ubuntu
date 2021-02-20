7316c841c6c13da7a07e03b020d4bd6e69fe5dad
Michael Bagnall <mrbagnall@icloud.com>
2021-02-20 09:09:08 -0500

    Corrected build file

--- 18.04/php7.4/build.sh
+++ 18.04/php7.4/build.sh
@@ -2,5 +2,5 @@
 
 docker build . -t proboci/ubuntu:18.04-php7.4 --squash
 docker build . -t mbagnall/ubuntu:18.04-php7.4 --squash
-docker push mbagnall/ubuntu:18.04-php7.4
+docker push proboci/ubuntu:18.04-php7.4
 docker push mbagnall/ubuntu:18.04-php7.4
