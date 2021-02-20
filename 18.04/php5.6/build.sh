8fec1647cd846aeb24b09351efc4a3d62e21c4cb
Michael Bagnall <mrbagnall@icloud.com>
2021-02-20 13:38:55 -0500

    Add our build script

---
+++ 18.04/php5.6/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build . -t proboci/ubuntu:18.04-php8.0 --squash
+docker build . -t mbagnall/ubuntu:18.04-php8.0 --squash
+docker push proboci/ubuntu:18.04-php8.0
+docker push mbagnall/ubuntu:18.04-php8.0
