a9cf7c770110fd07e6f00d2c8cc573b37168f7fa
Michael Bagnall <mrbagnall@icloud.com>
2021-03-04 20:21:29 -0600

    Updated build script with the correct versions

--- 18.04/php5.6/build.sh
+++ 18.04/php5.6/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php8.0 --squash
-docker build . -t mbagnall/ubuntu:18.04-php8.0 --squash
-docker push proboci/ubuntu:18.04-php8.0
-docker push mbagnall/ubuntu:18.04-php8.0
+docker build . -t proboci/ubuntu:18.04-php5.6 --squash
+docker build . -t mbagnall/ubuntu:18.04-php5.6 --squash
+docker push proboci/ubuntu:18.04-php5.6
+docker push mbagnall/ubuntu:18.04-php5.6
