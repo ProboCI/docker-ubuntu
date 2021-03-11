def3ecda14c511ced2955fd4f71ac992ed6be113
Michael Bagnall <mrbagnall@icloud.com>
2021-03-10 18:23:50 -0600

    Updated build just to do proboci

--- 18.04/php7.0/build.sh
+++ 18.04/php7.0/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php7.0 --squash
-docker build . -t mbagnall/ubuntu:18.04-php7.0 --squash
+docker build . -t proboci/ubuntu:18.04-php7.0 --squash --no-cache
 docker push proboci/ubuntu:18.04-php7.0
-docker push mbagnall/ubuntu:18.04-php7.0
