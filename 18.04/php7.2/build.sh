da558dc78af709724bf287ac51e418904fa5b672
Michael Bagnall <mrbagnall@icloud.com>
2021-03-10 18:37:17 -0600

    Only push to probo dockerhub

--- 18.04/php7.2/build.sh
+++ 18.04/php7.2/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php7.2 --squash
-docker build . -t mbagnall/ubuntu:18.04-php7.2 --squash
+docker build . -t proboci/ubuntu:18.04-php7.2 --squash --no-cache
 docker push proboci/ubuntu:18.04-php7.2
-docker push mbagnall/ubuntu:18.04-php7.2
