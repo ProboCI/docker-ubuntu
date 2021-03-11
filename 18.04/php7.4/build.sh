e19d79e488da03eba7cd297096b3944b1a8b2641
Michael Bagnall <mrbagnall@icloud.com>
2021-03-10 18:57:53 -0600

    Build just to proboci

--- 18.04/php7.4/build.sh
+++ 18.04/php7.4/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php7.4 --squash
-docker build . -t mbagnall/ubuntu:18.04-php7.4 --squash
+docker build . -t proboci/ubuntu:18.04-php7.4 --squash --no-cache
 docker push proboci/ubuntu:18.04-php7.4
-docker push mbagnall/ubuntu:18.04-php7.4
