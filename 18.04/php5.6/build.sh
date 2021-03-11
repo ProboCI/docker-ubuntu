19f1887a6a2e90b41222736283b7207f5f04016d
Michael Bagnall <mrbagnall@icloud.com>
2021-03-10 18:11:36 -0600

    Remove mbagnall builds

--- 18.04/php5.6/build.sh
+++ 18.04/php5.6/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php5.6 --squash
-docker build . -t mbagnall/ubuntu:18.04-php5.6 --squash
+docker build . -t proboci/ubuntu:18.04-php5.6 --squash --no-cache
 docker push proboci/ubuntu:18.04-php5.6
-docker push mbagnall/ubuntu:18.04-php5.6
