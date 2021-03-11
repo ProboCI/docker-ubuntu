55379fe5af03f613edb0c9b7d0f1bd2cf889a44b
Michael Bagnall <mrbagnall@icloud.com>
2021-03-10 18:32:15 -0600

    Update to commit to probo only

--- 18.04/php7.1/build.sh
+++ 18.04/php7.1/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php7.1 --squash
-docker build . -t mbagnall/ubuntu:18.04-php7.1 --squash
+docker build . -t proboci/ubuntu:18.04-php7.1 --squash --no-cache
 docker push proboci/ubuntu:18.04-php7.1
-docker push mbagnall/ubuntu:18.04-php7.1
