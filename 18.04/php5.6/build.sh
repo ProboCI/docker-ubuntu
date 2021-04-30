df03c1484a26eee90d16e832a403492f6607113c
Michael Bagnall <mrbagnall@icloud.com>
2021-04-29 22:20:19 -0400

    Add rsync

--- 18.04/php5.6/build.sh
+++ 18.04/php5.6/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php5.6 --squash --no-cache
+docker build . -t proboci/ubuntu:18.04-php5.6
+docker build . -t mbagnall/ubuntu:18.04-php5.6
 docker push proboci/ubuntu:18.04-php5.6
+docker push mbagnall/ubuntu:18.04-php5.6
