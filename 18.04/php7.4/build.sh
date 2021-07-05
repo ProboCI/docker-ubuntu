adde3e384203f91d23eb23f5ee52719d72189216
Michael Bagnall <mrbagnall@icloud.com>
2021-07-04 22:33:31 -0400

    Remove mbagnall and add no-cache

--- 18.04/php7.4/build.sh
+++ 18.04/php7.4/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker build . -t proboci/ubuntu:18.04-php7.4
-docker build . -t mbagnall/ubuntu:18.04-php7.4
+docker build . -t proboci/ubuntu:18.04-php7.4 --no-cache
 docker push proboci/ubuntu:18.04-php7.4
-docker push mbagnall/ubuntu:18.04-php7.4
