ee0bcabdbc000d2e5187cbeac620194ade7da2f6
Michael Bagnall <mrbagnall@icloud.com>
2021-08-08 12:06:53 -0500

    Cleanup of old cruft

--- 18.04/php7.3/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build . -t proboci/ubuntu:18.04-php7.3 --squash
-docker build . -t mbagnall/ubuntu:18.04-php7.3 --squash
-docker push mbagnall/ubuntu:18.04-php7.3
-docker push proboci/ubuntu:18.04-php7.3
