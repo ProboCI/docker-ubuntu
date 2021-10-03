f2eaafe9e2bef05bbadcb477091c1a951b9643f7
Michael Bagnall <mrbagnall@icloud.com>
2021-10-03 08:59:43 -0500

    Add 5.6

---
+++ 20.04/php5.6-mysql5.7/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+architecture=`arch`
+if [ ${architecture} = 'arm64' ]; then
+  echo "ARM64 Processor - Using buildx."
+  if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:php5.6-mysql5.7 --push .
+  else
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:php5.6-mysql5.7 .
+  fi
+else
+  echo "x86_64 Processor - using build."
+  docker build . -t proboci/ubuntu:php5.6-mysql5.7
+
+  if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
+    docker push proboci/ubuntu:php5.6-mysql5.7
+  fi
+fi
