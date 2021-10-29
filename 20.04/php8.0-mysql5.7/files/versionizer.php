29f30be9af97ffefbb09fae1ab31ad3955b8e32c
Michael Bagnall <mrbagnall@icloud.com>
2021-10-29 18:18:11 -0500

    Update versionizer

--- 20.04/php8.0-mysql5.7/files/versionizer.php
+++ 20.04/php8.0-mysql5.7/files/versionizer.php
@@ -17,7 +17,8 @@ $composer_version = `composer --version`;
 $drush_version = `drush --version`;
 $node_version = `node --version`;
 $wp_cli_version = `wp --version --allow-root`;
-// $terminus_version = `terminus --version`;
+$acli_version = `acli --version`;
+$terminus_version = `terminus --version`;
 
 print "\n\n";
 print "Probo.CI - Software Versions Report:\n";
@@ -33,5 +34,6 @@ print $composer_version;
 print $drush_version;
 print "Node: " . $node_version;
 print "WordPress CLI: " . $wp_cli_version;
-// print "Pantheon " . $terminus_version;
+print "Pantheon " . $terminus_version;
+print "Acquia CLI: " . $acli_version;
 print "\n\n";
