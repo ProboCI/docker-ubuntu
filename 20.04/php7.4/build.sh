06fe43036e58082ecfb30a61aa37f41763fb82e6
Michael Bagnall <mrbagnall@icloud.com>
2021-11-28 17:36:44 -0600

    Updates to search_api_solr to 4.2.2 and Chrome/Chrome Driver to 96

--- 20.04/php7.4/build.sh
+++ 20.04/php7.4/build.sh
@@ -4,13 +4,13 @@ architecture=`arch`
 if [ ${architecture} = 'arm64' ]; then
   echo "ARM64 Processor - Using buildx."
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:20.04-php7.4-mysql8.0 --push .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:20.04-php7.4 --push .
   else
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:20.04-php7.4-mysql8.0 .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:20.04-php7.4 .
   fi
 else
   echo "x86_64 Processor - using build."
-  docker build . -t proboci/ubuntu:20.04-php7.4-mysql8.0
+  docker build . -t proboci/ubuntu:20.04-php7.4
 
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
     docker push proboci/ubuntu:20.04-php7.4-mysql8.0
