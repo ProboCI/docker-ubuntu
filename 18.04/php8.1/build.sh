442db11df00b9ba8ceee5042029b35aa820e8cd5
Michael Bagnall <mrbagnall@icloud.com>
2021-11-29 21:11:59 -0600

    Add PHP 8.1 for Ubuntu 18.04

---
+++ 18.04/php8.1/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+architecture=`arch`
+if [ ${architecture} = 'arm64' ]; then
+  echo "ARM64 Processor - Using buildx."
+  if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php8.1 --push .
+  else
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php8.1 .
+  fi
+else
+  echo "x86_64 Processor - using build."
+  docker build . -t proboci/ubuntu:18.04-php8.1
+
+  if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
+    docker push proboci/ubuntu:18.04-php8.1
+  fi
+fi
