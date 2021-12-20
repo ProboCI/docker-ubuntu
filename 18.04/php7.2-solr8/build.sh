490fe7c1e24011142261dad764d96ee46bbd120b
Michael Bagnall <mrbagnall@icloud.com>
2021-12-20 13:51:16 -0600

    Update build scripts

--- 18.04/php7.2-solr8/build.sh
+++ 18.04/php7.2-solr8/build.sh
@@ -4,15 +4,15 @@ architecture=`arch`
 if [ ${architecture} = 'arm64' ]; then
   echo "ARM64 Processor - Using buildx."
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2 --push .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2-solr8 --push .
   else
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2 .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2-solr8 .
   fi
 else
   echo "x86_64 Processor - using build."
-  docker build . -t proboci/ubuntu:18.04-php7.2
+  docker build . -t proboci/ubuntu:18.04-php7.2-solr8
 
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-    docker push proboci/ubuntu:18.04-php7.2
+    docker push proboci/ubuntu:18.04-php7.2-solr8
   fi
 fi
