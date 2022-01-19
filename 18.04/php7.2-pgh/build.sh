722e0e5577a18f37d1a99f3276fab09f4559c83f
Michael R. Bagnall <mrbagnall@icloud.com>
2022-01-19 07:51:02 +0000

    Bring our PGP images up with our Probo changes.

--- 18.04/php7.2-pgh/build.sh
+++ 18.04/php7.2-pgh/build.sh
@@ -4,15 +4,15 @@ architecture=`arch`
 if [ ${architecture} = 'arm64' ]; then
   echo "ARM64 Processor - Using buildx."
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2-solr7 --push .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2a --push .
   else
-    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2-solr7 .
+    docker buildx build --platform linux/amd64 -t proboci/ubuntu:18.04-php7.2a .
   fi
 else
   echo "x86_64 Processor - using build."
-  docker build . -t proboci/ubuntu:18.04-php7.2-solr7
+  docker build . -t proboci/ubuntu:18.04-php7.2a
 
   if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-    docker push proboci/ubuntu:18.04-php7.2-solr7
+    docker push proboci/ubuntu:18.04-php7.2a
   fi
 fi
