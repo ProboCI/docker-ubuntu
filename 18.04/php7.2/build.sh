79fa397fd1398a92edc3d18916450778c1323de9
Michael Bagnall <mrbagnall@icloud.com>
2022-08-07 18:49:58 -0500

    Fixed a typo around php7.2 building script

--- 18.04/php7.2/build.sh
+++ 18.04/php7.2/build.sh
@@ -5,7 +5,7 @@ echo -n "Ubuntu 18.04 - PHP 7.2 ProboCI Build: "
 docker build . -q -t proboci/ubuntu:18.04-php7.2
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
   echo -n "Pushing to DockerHub: "
-  docker push -q probci/ubuntu:18.04-php7.2
+  docker push -q proboci/ubuntu:18.04-php7.2
 fi
 
 echo ""
