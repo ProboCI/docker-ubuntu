3b091b2ecd71116cc964a78b3dbc6203aff5c36f
Michael Bagnall <mrbagnall@icloud.com>
2022-09-22 19:40:27 -0500

    Work on 22.04

--- 22.04/php8.0/build.sh
+++ 22.04/php8.0/build.sh
@@ -2,7 +2,7 @@
 
 echo -n "Ubuntu 22.04 - PHP 8.0 ProboCI Build: "
 
-docker buildx build --platform linux/amd64 -t proboci/ubuntu:22.04-php8.0 --push .
+# docker buildx build --platform linux/amd64 -t proboci/ubuntu:22.04-php8.0 --push .
 
 docker build . -q -t proboci/ubuntu:22.04-php8.0
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
