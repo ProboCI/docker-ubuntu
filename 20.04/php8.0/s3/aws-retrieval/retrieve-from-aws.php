e5d12fb5b76b050b3781aaed3cc254d28c1daef4
Michael Bagnall <michael@bagnall.io>
2023-01-28 07:39:23 -0600

    Add our pull from s3 to the docker container

---
+++ 20.04/php8.0/s3/aws-retrieval/retrieve-from-aws.php
@@ -0,0 +1,62 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+use Aws\S3\ObjectUploader;
+use Aws\S3\ListObjects;
+use Aws\S3\DeleteObject;;
+
+ini_set('memory_limit', '2G');
+
+$output = $argv[1];
+
+$aws_key = getenv('AWS_KEY');
+$aws_secret = getenv('AWS_SECRET');
+$aws_bucket = getenv('AWS_BUCKET');
+
+if (!empty($aws_key) && !empty($aws_secret)) {
+  $s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1',
+    'use_path_style_endpoint' => true,
+    'credentials' => [
+      'key'    => $aws_key,
+      'secret' => $aws_secret,
+    ],
+  ]);
+}
+else {
+  print "Unable to connect to aws.";
+  exit();
+}
+
+$files = ['cancel'];
+try {
+  $results = $s3->getPaginator('ListObjects', [
+    'Bucket' => $aws_bucket
+  ]);
+
+} catch (S3Exception $e) {
+  echo $e->getMessage();
+}
+if (!empty($results)) {
+  foreach ($results as $result) {
+    foreach ($result['Contents'] as $key => $content) {
+      $files[] = $content['Key'];
+    }
+  }
+}
+
+$file_to_retrieve = array_pop($files);
+
+try {
+  $result = $s3->getObject([
+    'Bucket' => $aws_bucket,
+    'Key'    => $file_to_retrieve,
+  ]);
+  file_put_contents('/assets/' . $output, $result['Body']);
+} catch (S3Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
