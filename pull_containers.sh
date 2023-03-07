25a0c449f2ce432c01537d9b4835057c08583feb
Michael Bagnall <michael@bagnall.io>
2023-03-07 13:05:31 -0600

    Update terminus

--- pull_containers.sh
+++ pull_containers.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-docker pull proboci/ubuntu:18.04-php5.6
-docker pull proboci/ubuntu:18.04-php7.0
-docker pull proboci/ubuntu:18.04-php7.1
-docker pull proboci/ubuntu:18.04-php7.2
-docker pull proboci/ubuntu:18.04-php7.3
-docker pull proboci/ubuntu:18.04-php7.4
-docker pull proboci/ubuntu:18.04-php8.0
+# 18.04 images are no longer being maintained by Probo.
+# docker pull proboci/ubuntu:18.04-php5.6
+# docker pull proboci/ubuntu:18.04-php7.0
+# docker pull proboci/ubuntu:18.04-php7.1
+# docker pull proboci/ubuntu:18.04-php7.2
+# docker pull proboci/ubuntu:18.04-php7.3
+# docker pull proboci/ubuntu:18.04-php7.4
+# docker pull proboci/ubuntu:18.04-php8.0
+# docker pull proboci/ubuntu:18.04-php8.1
 docker pull proboci/ubuntu:20.04-php7.4
 docker pull proboci/ubuntu:20.04-php8.0
 docker pull proboci/ubuntu:20.04-php8.1
 docker pull proboci/ubuntu:22.04-php7.4
 docker pull proboci/ubuntu:22.04-php8.0
 docker pull proboci/ubuntu:22.04-php8.1
+docker pull proboci/ubuntu:22.04-php8.2
+docker pull proboci/alpine:php7.4
+docker pull proboci/alpine:php8.0
+docker pull proboci/alpine:php8.1
