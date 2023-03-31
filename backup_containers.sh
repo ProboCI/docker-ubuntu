aecdc411254a48b08112efe87e842ca8e73a65de
Michael Bagnall <michael@bagnall.io>
2023-03-31 16:45:56 -0500

    Clean up docker files and make sure 22.04 has MariaDB

--- backup_containers.sh
+++ backup_containers.sh
@@ -3,45 +3,79 @@
 echo "Backing up 18.04-php5.6"
 docker save proboci/ubuntu:18.04-php5.6 > image-backups/18.04-php5.6-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php5.6-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php7.0"
 docker save proboci/ubuntu:18.04-php7.0 > image-backups/18.04-php7.0-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php7.0-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php7.1"
 docker save proboci/ubuntu:18.04-php7.1 > image-backups/18.04-php7.1-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php7.1-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php7.2"
 docker save proboci/ubuntu:18.04-php7.2 > image-backups/18.04-php7.2-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php7.2-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php7.3"
 docker save proboci/ubuntu:18.04-php7.3 > image-backups/18.04-php7.3-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php7.3-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php7.4"
 docker save proboci/ubuntu:18.04-php7.4 > image-backups/18.04-php7.4-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php7.4-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php8.0"
 docker save proboci/ubuntu:18.04-php8.0 > image-backups/18.04-php8.0-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php8.0-$(date +%Y-%m-%d).tar
+
 echo "Backing up 18.04-php8.1"
 docker save proboci/ubuntu:18.04-php8.1 > image-backups/18.04-php8.1-$(date +%Y-%m-%d).tar
 gzip image-backups/18.04-php8.1-$(date +%Y-%m-%d).tar
+
 echo "Backing up 20.04-php7.4"
 docker save proboci/ubuntu:20.04-php7.4 > image-backups/20.04-php7.4-$(date +%Y-%m-%d).tar
 gzip image-backups/20.04-php7.4-$(date +%Y-%m-%d).tar
+
 echo "Backing up 20.04-php8.0"
 docker save proboci/ubuntu:20.04-php8.0 > image-backups/20.04-php8.0-$(date +%Y-%m-%d).tar
 gzip image-backups/20.04-php8.0-$(date +%Y-%m-%d).tar
+
 echo "Backing up 20.04-php8.1"
 docker save proboci/ubuntu:20.04-php8.1 > image-backups/20.04-php8.1-$(date +%Y-%m-%d).tar
 gzip image-backups/20.04-php8.1-$(date +%Y-%m-%d).tar
+
+echo "Backing up 20.04-php8.2"
+docker save proboci/ubuntu:20.04-php8.2 > image-backups/20.04-php8.2-$(date +%Y-%m-%d).tar
+gzip image-backups/20.04-php8.2-$(date +%Y-%m-%d).tar
+
 echo "Backing up 22.04-php7.4"
 docker save proboci/ubuntu:22.04-php7.4 > image-backups/22.04-php7.4-$(date +%Y-%m-%d).tar
 gzip image-backups/22.04-php7.4-$(date +%Y-%m-%d).tar
+
 echo "Backing up 22.04-php8.0"
 docker save proboci/ubuntu:22.04-php8.0 > image-backups/22.04-php8.0-$(date +%Y-%m-%d).tar
 gzip image-backups/22.04-php8.0-$(date +%Y-%m-%d).tar
+
 echo "Backing up 22.04-php8.1"
 docker save proboci/ubuntu:22.04-php8.1 > image-backups/22.04-php8.1-$(date +%Y-%m-%d).tar
 gzip image-backups/22.04-php8.1-$(date +%Y-%m-%d).tar
+
 echo "Backing up 22.04-php8.2"
 docker save proboci/ubuntu:22.04-php8.2 > image-backups/22.04-php8.2-$(date +%Y-%m-%d).tar
 gzip image-backups/22.04-php8.2-$(date +%Y-%m-%d).tar
+
+echo "Backing up Alpine PHP 7.4"
+docker save proboci/alpine:php7.4 > image-backups/alpine-php7.4-$(date +%Y-%m-%d).tar
+gzip image-backups/alpine-php7.4-$(date +%Y-%m-%d).tar
+
+echo "Backing up Alpine PHP 8.0"
+docker save proboci/alpine:php8.0 > image-backups/alpine-php8.0-$(date +%Y-%m-%d).tar
+gzip image-backups/alpine-php8.0-$(date +%Y-%m-%d).tar
+
+echo "Backing up Alpine PHP 8.1"
+docker save proboci/alpine:php8.1 > image-backups/alpine-php8.1-$(date +%Y-%m-%d).tar
+gzip image-backups/alpine-php8.1-$(date +%Y-%m-%d).tar
+
+echo "Backing up Alpine PHP 8.2"
+docker save proboci/alpine:php8.2 > image-backups/alpine-php8.2-$(date +%Y-%m-%d).tar
+gzip image-backups/alpine-php8.2-$(date +%Y-%m-%d).tar
--- pull_containers.sh
+++ pull_containers.sh
@@ -19,3 +19,4 @@ docker pull proboci/ubuntu:22.04-php8.2
 docker pull proboci/alpine:php7.4
 docker pull proboci/alpine:php8.0
 docker pull proboci/alpine:php8.1
+docker pull proboci/alpine:php8.2
