20873226b3c669be1e3bf6c6d0bc357d156180bb
Michael Bagnall <mrbagnall@icloud.com>
2023-10-27 18:30:28 -0500

    Fix build process

--- 22.04/php8.3/build.sh
+++ 22.04/php8.3/build.sh
@@ -2,7 +2,7 @@
 
 echo -n "Ubuntu 22.04 - PHP 8.3 ProboCI Build: "
 
-docker build . -q -t proboci/ubuntu:22.04-php8.3 ${2}
+docker build . -q -t proboci/ubuntu:22.04-php8.3
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
   echo -n "Pushing to DockerHub: "
   docker push -q proboci/ubuntu:22.04-php8.3
--- build.sh
+++ build.sh
@@ -26,19 +26,19 @@
 
 # cd ../../
 
-# cd ./20.04/php7.4/
-# ./build.sh $1 $2
+cd ./20.04/php7.4/
+./build.sh $1 $2
 
-# cd ../../20.04/php8.0/
-# ./build.sh $1 $2
+cd ../../20.04/php8.0/
+./build.sh $1 $2
 
-# cd ../../20.04/php8.1/
-# ./build.sh $1 $2
+cd ../../20.04/php8.1/
+./build.sh $1 $2
 
-# cd ../../20.04/php8.2/
-# ./build.sh $1 $2
+cd ../../20.04/php8.2/
+./build.sh $1 $2
 
-# cd ../../
+cd ../../
 
 cd ./22.04/php7.4/
 ./build.sh $1 $2
