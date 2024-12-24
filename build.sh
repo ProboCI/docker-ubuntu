c135b268fc382a429bf220095d8296eef1e17cf9
Michael Bagnall <mrbagnall@icloud.com>
2024-12-24 16:52:45 -0600

    Get 24.04 PHP 8.4 build trimmed down and configured

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd ./20.04/php7.4/
-./build.sh $1 $2
+# cd ./20.04/php7.4/
+# ./build.sh $1 $2
 
-cd ../../20.04/php8.0/
-./build.sh $1 $2
+# cd ../../20.04/php8.0/
+# ./build.sh $1 $2
 
-cd ../../20.04/php8.1/
-./build.sh $1 $2
+# cd ../../20.04/php8.1/
+# ./build.sh $1 $2
 
-cd ../../20.04/php8.2/
-./build.sh $1 $2
+# cd ../../20.04/php8.2/
+# ./build.sh $1 $2
 
-cd ../../20.04/php8.3/
-./build.sh $1 $2
+# cd ../../20.04/php8.3/
+# ./build.sh $1 $2
 
-cd ./../22.04/php7.4/
+cd ./22.04/php7.4/
 ./build.sh $1 $2
 
 cd ../../22.04/php8.0/
@@ -33,4 +33,7 @@ cd ../../22.04/php8.3/
 cd ../../22.04/php8.4/
 ./build.sh $1 $2
 
+cd ../../24.04/php8.4/
+./build.sh $1 $2
+s
 cd ../../
