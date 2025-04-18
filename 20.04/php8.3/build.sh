9e8916ee49f0287179b10f9d42023742a2a2ec0b
Michael Bagnall <mrbagnall@icloud.com>
2025-04-17 19:06:13 -0500

    Update Search API SOLR for Drupal 7 and 8+

--- 20.04/php8.3/build.sh
+++ 20.04/php8.3/build.sh
@@ -3,10 +3,21 @@
 echo "---------------------------------------------------------------"
 echo "Ubuntu 20.04 - PHP 8.3 ProboCI Build: "
 echo "---------------------------------------------------------------"
-docker build . -t proboci/ubuntu:20.04-php8.3 ${2}
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
+  echo "Building Production"
+  echo "---------------------------------------------------------------"
+  docker build . -q -t proboci/ubuntu:20.04-php8.3 ${2}
   echo "---------------------------------------------------------------"
   echo "Pushing to DockerHub: "
-  docker push proboci/ubuntu:20.04-php8.3
+  docker push -q proboci/ubuntu:20.04-php8.3
+  echo "---------------------------------------------------------------"
 fi
-echo "---------------------------------------------------------------"
+if [[ ${1} = 'beta' ]]; then
+  echo "Building Beta"
+  echo "---------------------------------------------------------------"
+  docker build . -q -t proboci/ubuntu:20.04-php8.3-beta ${2}
+  echo "---------------------------------------------------------------"
+  echo "Pushing *BETA* to DockerHub: "
+  docker push -q proboci/ubuntu:20.04-php8.3-beta
+  echo "---------------------------------------------------------------"
+fi
